d5a4413a005846c3f03dc9169ff8b32a0f3689f9
Mohamed Safouan Besrour <besrour_2010@live.com>
2022-12-12 15:41:33 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -17,13 +17,13 @@ class TUI {
 
         if (!preg_match($regex, $this->value)) return false;
 
-		$array = str_split(substr($this->value, 0, -1), 1);
+	$array = str_split(substr($this->value, 0, -1), 1);
 
-		$index = -1;
+	$index = -1;
 
         $ctrlKey = array_reduce($array, function($prevResult, $currentValue) use (&$index, &$array) {
-            $index++;
-			return $prevResult + $currentValue * (count($array) - $index);
+            	$index++;
+		return $prevResult + $currentValue * (count($array) - $index);
         }, 0) % 23;
 
         return substr($this->value, -1) == str_split(self::alphabet)[$ctrlKey];
